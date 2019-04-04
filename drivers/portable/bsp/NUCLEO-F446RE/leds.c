82b425e9751112dce97b0716a990e1ac9cab549d
damianex19 <damiankarbownik19@vp.pl>
2019-04-04 19:19:06 +0200

    LED INIT
    Signed-off-by: damianex19 <damiankarbownik19@vp.pl>

--- drivers/portable/bsp/NUCLEO-F446RE/leds.c
+++ drivers/portable/bsp/NUCLEO-F446RE/leds.c
@@ -15,10 +15,21 @@
 
 void leds_init(uint32_t leds)
 {
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	GPIO_InitTypeDef gpio;
+	gpio.Pin=GPIO_PIN_5;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull=GPIO_NOPULL;
+	gpio.Speed=GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA,&gpio);
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_SET);
 }
 
 void leds_deinit(uint32_t leds)
 {
+
+HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_RESET);
+
 }
 
 void leds_set(uint32_t leds, led_state_t state)
@@ -32,6 +43,7 @@ led_state_t leds_get(uint32_t leds)
 
 void leds_toggle( uint32_t leds )
 {
+
 }
 
 void leds_on( uint32_t leds )
